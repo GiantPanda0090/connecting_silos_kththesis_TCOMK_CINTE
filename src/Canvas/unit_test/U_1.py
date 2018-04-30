6c8e68ff0854158cf22a4c799126bebd6050c00d
QILI <liqischool@gmail.com>
2018-04-30 16:05:45 +0200

    obtain all student list

--- src/Canvas/unit_test/U_1.py
+++ src/Canvas/unit_test/U_1.py
@@ -17,7 +17,7 @@ Expected_kth_canvas= config.kth_canvas
 Expected_kth_canvas_thesis_course_id = config.thesis_course_id #sandbox
 #assignment_id 24565 testing 1; 24566 testing 2 ; 24567 testing 3;
 Expcted_proporsal_assignment_id=config.proporsal_assignment_id #test_1
-Expected_thesis_assignment_id=config.Thesis_assignment_id #test_1
+Expected_thesis_assignment_id=config.Thesis_assignment_id #test_2
 
 
 def main(link,document_type):
---
+++ src/Canvas/unit_test/U_2.py
@@ -0,0 +1,12 @@
+import config
+#initialize test enviorment
+Expected_kth_canvas= config.kth_canvas
+#course_id 2139(sandbox)
+Expected_kth_canvas_thesis_course_id = config.thesis_course_id #sandbox
+#assignment_id 24565 testing 1; 24566 testing 2 ; 24567 testing 3;
+Expcted_proporsal_assignment_id=config.proporsal_assignment_id #test_1
+Expected_thesis_assignment_id=config.Thesis_assignment_id #test_2
+
+
+def main(link,document_type):
+
