58a8afc71ff8a1b04c01da559d2019c3c5332317
QILI <liqischool@gmail.com>
2018-05-01 11:16:24 +0200

    dynamic read

--- src/run_proporsal.sh
+++ src/run_proporsal.sh
@@ -2,6 +2,11 @@
 #add geckodriver path 
 #run the proporsal phase
 
+path=$(pwd)
+rm -rf $path/../ffdriver
+mkdir $path/../ffdriver
+rm -rf $path/../Source
+mkdir $path/../Source
 source add_path.sh 
-python start.py $1 $2 $3 $4
+python start_proporsal.py $1 $2 $3 $4
 
--- src/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#add geckodriver path 
-#test script.... delete before deployed
-
-source add_path.sh 
-python start.py 2139 24565 shivabp IBstar1998
-
